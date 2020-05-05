7899e03cea4b40a63fafb1fc0a9abd1f3905d088
korimart <korimart2019@gmail.com>
2020-05-05 20:18:10 +0900

    Bit Stream

--- app/src/main/java/com/example/myapplication/Core.java
+++ app/src/main/java/com/example/myapplication/Core.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import android.content.Context;
 import android.os.SystemClock;
 
+import java.io.IOException;
+
 /**
  * 앱이 사용하는 여러 클래스를 초기화하고 작동순서대로 호출합니다.
  * 게임루프가 들어있습니다.
--- app/src/main/java/com/example/myapplication/TempOutputBitStream.java
+++ app/src/main/java/com/example/myapplication/TempOutputBitStream.java
@@ -1,11 +1,18 @@
 package com.example.myapplication;
 
+import java.io.IOException;
+
 public class TempOutputBitStream implements OutputBitStream {
     private byte[] _buffer = new byte[1300];
 
     @Override
-    public void writeBits(byte[] data, int numBits) {
-        // TODO
+    public void write(byte[] data, int numBits) throws IOException {
+
+    }
+
+    @Override
+    public void write(int data, int numBits) throws IOException {
+
     }
 
     @Override
