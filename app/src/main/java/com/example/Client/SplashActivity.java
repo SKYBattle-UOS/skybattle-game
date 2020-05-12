bdf1d7018fbddc2c04bae2a38e05824438d21d3e
korimart <korimart2019@gmail.com>
2020-05-12 19:05:29 +0900

    nothing

--- app/src/main/java/com/example/Client/SplashActivity.java
+++ app/src/main/java/com/example/Client/SplashActivity.java
@@ -23,21 +23,6 @@ public class SplashActivity  extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        // 권한 부분 어떻게 할것인지?
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            //권한이 없을 경우 최초 권한 요청 또는 사용자에 의한 재요청 확인
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION) &&
-                    ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                // 권한 재요청
-                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
-                return;
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
-                return;
-            }
-        }
-
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
