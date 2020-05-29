66b141442fea9e089f587f226f3a6a59b75af594
korimart <korimart2019@gmail.com>
2020-05-30 02:56:20 +0900

    nothing

--- app/src/main/java/Host/GlobalWazakWazakHost.java
+++ app/src/main/java/Host/GlobalWazakWazakHost.java
@@ -4,6 +4,7 @@ import Common.ImageType;
 
 import Common.GameObject;
 import Common.GlobalWazakWazakCommon;
+import Common.ItemHost;
 import Common.Pickable;
 import Common.PlayerProperty;
 import Common.Util;
@@ -13,7 +14,7 @@ public class GlobalWazakWazakHost extends GlobalWazakWazakCommon {
     public void cast(GameObject caster) {
         MatchHost match = CoreHost.get().getMatch();
 
-        GameObject spawned = match.createGameObject(Util.ItemClassId, true);
+        ItemHost spawned = (ItemHost) match.createGameObject(Util.ItemClassId, true);
         spawned.setName("원격 와작와작 지뢰");
         spawned.setPosition(_lat, _lon);
         spawned.setLook(ImageType.MARKER);
@@ -21,7 +22,7 @@ public class GlobalWazakWazakHost extends GlobalWazakWazakCommon {
         match.getWorldSetterHost()
                 .generateUpdateInstruction(caster.getNetworkId(), PlayerProperty.skillDirtyFlag);
         match.setTimer(() -> {
-            if (!((Pickable) spawned).isPickedUp())
+            if (!spawned.isPickedUp())
                 spawned.scheduleDeath();
         }, 10);
     }
