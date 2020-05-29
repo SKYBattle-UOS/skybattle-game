50bcfb58cd978a08564d760dd3754e975fed7dff
korimart <korimart2019@gmail.com>
2020-05-29 10:58:02 +0900

    nothing

--- app/src/main/java/com/example/Client/InGameFragment.java
+++ app/src/main/java/com/example/Client/InGameFragment.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleObserver;
 
 import Common.CoordinateSkill;
 import Common.PlayerTargetSkill;
@@ -21,6 +22,7 @@ public class InGameFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        getLifecycle().addObserver((LifecycleObserver) Core.get().getUIManager());
         return inflater.inflate(R.layout.fragment_ingame, container, false);
     }
 
